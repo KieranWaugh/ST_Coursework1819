76b625bec8e96297110ec3a3bf3e6c41fc055700
kieran waugh <kieran.waugh98@gmail.com>
2019-03-22 15:52:23 +0000

    started Task 1

--- SourceCode/src/Task1_Functional.java
+++ SourceCode/src/Task1_Functional.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import st.Parser;
@@ -8,5 +9,26 @@ import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class Task1_Functional {
-
+	
+	private Parser parser;
+	 
+	@Before
+	public void start() {
+		parser = new Parser();
+	}
+	
+	// Spec 1
+	
+	@Test
+	public void task1_spec1() {
+		parser.add("test", "t", Parser.STRING);
+		parser.add("test", "t", Parser.INTEGER);
+		parser.parse("--test 1");
+		assertEquals(parser.getInteger("t"), 1);
+		
+	}
+	
+	
+	
+	
 }
